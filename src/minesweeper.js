3f1e713cab0b27c56a046eee35fda367c10c0a7a
Tobias Gulbrandsen Waaler <Tobias.Gulbrandsen.Waaler@nrk.no>
2015-11-17 13:39:39 +0100

    first test

---
+++ src/minesweeper.js
@@ -0,0 +1,3 @@
+export function createGame({cols, rows, mines}){
+  
+}
--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,11 @@
-import {assert} from 'chai';
+import {expect} from 'chai';
+import minesweeper from '../src/minesweeper';
 
-describe('test', () => {
-  it('test', () => {
-    assert.equal(1,2);
-  });
+describe('Minesweeper', () => {
+  it('createGame returns an initial game', () => {
+    const {cols, rows, tiles} = minesweeper.createGame(7, 7, 9);
+    expect(cols).to.equal(7);
+    expect(rows).to.equal(7);
+    expect(tiles).to.have.length(7);
+  })
 });
